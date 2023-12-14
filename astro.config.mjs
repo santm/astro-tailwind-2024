7647ce3b82167712beda4fc246932027aea4984d
santm <santm@hotmail.com>
2023-12-14 21:50:34 +0530

    first test page with tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
-
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), sitemap()]
+  integrations: [tailwind(), sitemap(), mdx()],
+	 prefetch: true
 });
