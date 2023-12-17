cfcd03bb4bad149fab8b75b014097ea582b5d3c1
santm <santm@hotmail.com>
2023-12-17 08:08:16 +0530

    about page done ; but again the theme does not sticks

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://santm.com',
   integrations: [tailwind(), sitemap(), mdx()],
 	 prefetch: true
 });
