c44373c4791edc10a1c04f5115a6b6aef454e495
santm <santm@hotmail.com>
2023-12-17 15:15:46 +0530

    removed preline

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./node_modules/preline/preline.js'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
 		extend: {
 			fontFamily: {
-				// heading: ['Montserrat', 'sans-serif'],
-				// code: ['Inconsolata', 'monospace'],
 				sans: [
 					'"Inter var", sans-serif',
 					{
@@ -15,23 +13,9 @@ module.exports = {
 					},
 				  ],
 			  },
-			// colors: {
-			// 	// body: "rgb(var(--color-bg))",
-			// 	// "box-bg": "rgb(var(--color-box))",
-			// 	// "box-shadow": "rgb(var(--box-sd))",
-			// 	// "box-border": "rgb(var(--box-border))",
-			// 	// primary: "#1d4ed8",
-			// 	// "heading-1": "rgb(var(--heading-1))",
-			// 	// "heading-2": "rgb(var(--heading-2))",
-			// 	// "heading-3": "rgb(var(--heading-3))",
-			// },
-			// screens:{
-			// 	// midmd:"880px"
-			// }
 		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
-		require('preline/plugin'),
 	],
 }
