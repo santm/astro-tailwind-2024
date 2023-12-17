01b66d044314ee4db57e7c66f8e078a47b80c220
santm <santm@hotmail.com>
2023-12-17 13:40:57 +0530

    header is really broken .. :(

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,19 +4,30 @@ module.exports = {
 	darkMode: "class",
 	theme: {
 		extend: {
-			colors: {
-				// body: "rgb(var(--color-bg))",
-				// "box-bg": "rgb(var(--color-box))",
-				// "box-shadow": "rgb(var(--box-sd))",
-				// "box-border": "rgb(var(--box-border))",
-				// primary: "#1d4ed8",
-				// "heading-1": "rgb(var(--heading-1))",
-				// "heading-2": "rgb(var(--heading-2))",
-				// "heading-3": "rgb(var(--heading-3))",
-			},
-			screens:{
-				// midmd:"880px"
-			}
+			fontFamily: {
+				// heading: ['Montserrat', 'sans-serif'],
+				// code: ['Inconsolata', 'monospace'],
+				sans: [
+					'"Inter var", sans-serif',
+					{
+					  fontFeatureSettings: '"cv11", "ss01"',
+					  fontVariationSettings: '"opsz" 32'
+					},
+				  ],
+			  },
+			// colors: {
+			// 	// body: "rgb(var(--color-bg))",
+			// 	// "box-bg": "rgb(var(--color-box))",
+			// 	// "box-shadow": "rgb(var(--box-sd))",
+			// 	// "box-border": "rgb(var(--box-border))",
+			// 	// primary: "#1d4ed8",
+			// 	// "heading-1": "rgb(var(--heading-1))",
+			// 	// "heading-2": "rgb(var(--heading-2))",
+			// 	// "heading-3": "rgb(var(--heading-3))",
+			// },
+			// screens:{
+			// 	// midmd:"880px"
+			// }
 		},
 	},
 	plugins: [
