a67751c4b4d667baa5f5b0bf82b4e29b3162bb0c
santm <santm@hotmail.com>
2023-12-17 11:04:58 +0530

    no luck with theme switch, all are ok so far

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://santm.com',
-  integrations: [tailwind(), sitemap(), mdx()],
-	 prefetch: true
+  integrations: [tailwind(), sitemap(), mdx()]
+	//  prefetch: true
 });
