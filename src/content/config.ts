7fe413b16d6aa8e222c7cf94f57d0f27c26cc0d2
santm <santm@hotmail.com>
2023-12-22 20:57:39 +0530

    not such progress in whole day; need to read about the documenattaion in details

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
 import { z, defineCollection } from "astro:content";
 
-const blog = defineCollection({
+const blogCollection = defineCollection({
+  type: "content", // v2.5.0 and later
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
@@ -9,16 +10,28 @@ const blog = defineCollection({
       src: z.string().optional(),
       alt: z.string().optional(),
     }),
-    description: z
-      .string()
-      .max(
-        160,
-        "For best SEO results, please keep the description under 160 characters."
-      ).optional(),
+    description: z.string().max(160, "For best SEO results, please keep the description under 160 characters.").optional(),
     draft: z.boolean().default(false),
-    category: z.enum(["Travel", "Poem", "Technology", "General", "Food","Running"]),
+    category: z.enum(["Travel", "Poem", "Technology", "General", "Food", "Running"]),
     tags: z.array(z.string()),
   }),
 });
 
-export const collections = { blog };
+// // 3. Export a single `collections` object to register your collection(s)
+// export const collections = {
+//   blog: blogCollection,
+// };
+//
+const santCollection = defineCollection({
+  schema: ({ image }) => z.object({
+    coverAlt: z.string(),
+    cover: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+  sant: santCollection,
+};
