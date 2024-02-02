980941a1f04f1fe0ce4083890e26c858b5938cfe
santm <santm@hotmail.com>
2024-02-02 15:22:27 +0530

    added astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,28 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import remarkIt from 'markdown-it';
 import remarkToc from 'remark-toc';
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://santm.com',
-  integrations: [tailwind(), sitemap(), mdx()],
-	//  prefetch: true
+  integrations: [tailwind(), sitemap(), mdx(), icon()],
+  //  prefetch: true
   //trailingSlash: 'always', problem only - santm
-  markdown: {
-    // Applied to .md and .mdx files
-    remarkPlugins: [ [remarkToc, { heading: "contents"} ] ],
-    remarkPlugins: [remarkIt]
+  // markdown: {
+  //   // Applied to .md and .mdx files
+  //   remarkPlugins: [ [remarkToc, { heading: "contents"} ] ],
+  //   remarkPlugins: [remarkIt]
+  // }
+  renderers: [],
+  markdownOptions: {
+    render: ['@astrojs/markdown-remark', {
+      remarkPlugins: [
+      // Other remark plugins
+      ['remark-toc', {
+        ordered: true
+      }]]
+      // Rehype plugins…
+    }]
   }
 });
